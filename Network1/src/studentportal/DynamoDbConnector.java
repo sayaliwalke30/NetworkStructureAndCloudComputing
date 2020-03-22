530b897345f5df4dd6071e3b3a411f3f7284cd0d
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-21 19:57:52 -0700

    DynamoDbConnector

---
+++ Network1/src/studentportal/DynamoDbConnector.java
@@ -0,0 +1,27 @@
+package studentportal;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+public class DynamoDbConnector {
+	static AmazonDynamoDB dynamoDb ;
+	 
+	 public static void init() {
+		if (dynamoDb == null) {
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		credentialsProvider.getCredentials();
+		
+		dynamoDb = AmazonDynamoDBClientBuilder
+					.standard()
+					.withCredentials(credentialsProvider)
+					.withRegion("us-west-2")
+					.build();		
+		System.out.println("I created the client");
+		} 
+
+	}
+	 
+	 public AmazonDynamoDB getClient() {
+		 return dynamoDb;
+	 }
+}
