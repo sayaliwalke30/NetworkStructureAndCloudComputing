fb956ddb3c18d8c98b48b655ab0ebacba747e75e
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-23 13:49:16 -0700

    DynamoDbTrial

---
+++ Network1/src/studentportal/resources/AnnouncementResource.java
@@ -0,0 +1,76 @@
+package studentportal.resources;
+
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import studentportal.datamodel.Announcements;
+
+
+
+@Path("announcements")
+public class AnnouncementResource {
+	AnnouncementService a_Service=new AnnouncementService();
+	
+	//GET
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Announcements> getAllAnnoucements(){
+		return a_Service.getAllAnnouncements();
+	}
+	
+	//get an announcement by annoucementid and boardid
+	@GET
+	@Path("/{boardId}_{announcementId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Announcements getOneAnnoucement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId) {
+		return a_Service.getOneAnnouncement(announcementId, boardId);
+	}
+	
+	//get announcements by boardId
+	@GET
+	@Path("/board/{boardId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Announcements> getAnnouncementsByBoard(@PathParam("boardId")String boardId){
+		if(boardId==null) {
+			return a_Service.getAllAnnouncements();
+		}
+		return a_Service.getAnnoucementsByBoard(boardId);
+	}
+	
+	//POST
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Announcements addAnnouncement(Announcements announce) {
+		return a_Service.addAnnouncement(announce);
+	}
+	
+	//delete
+	@DELETE
+	@Path("/{boardId}_{announcementId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Announcements deleteAnnoucement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId) {
+		return a_Service.deleteAnnouncement(announcementId, boardId);
+	}
+	
+	//update
+	@PUT
+	@Path("/{boardId}_{annoucementId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Announcements updateAnnouncement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId,Announcements a) {
+		return a_Service.updateAnnouncement(announcementId, boardId, a);
+	}
+	
+	
+}
