e9ef4a50ef8452614c79e10a0bf4cabcaae4d277
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-23 23:10:15 -0700

    DynamoDb connection

--- Network1/src/studentportal/services/CourseService.java
+++ Network1/src/studentportal/services/CourseService.java
@@ -4,14 +4,31 @@ import java.util.ArrayList;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import studentportal.DynamoDbConnector;
 import studentportal.InMemoryDatabase;
 import studentportal.datamodel.Course;
+import studentportal.datamodel.Professor;
 
 public class CourseService {
-	static HashMap<Long, Course> course_Map = InMemoryDatabase.getCourseDB();
+	//static HashMap<Long, Course> course_Map = InMemoryDatabase.getCourseDB();
+	static DynamoDbConnector dynamoDb;
+	DynamoDBMapper course_Map;
+	
+	BoardService boardService=new BoardService();
+	public CourseService() {
+		dynamoDb=new DynamoDbConnector();
+		dynamoDb.init();
+		course_Map=new DynamoDBMapper(dynamoDb.getClient());
+		System.out.println("DynamoDb client initialized");
+	}
 
-	// Adding a Course
+	/*// Adding a Course
 	public void addCourse(String courseId, String courseName, String professorId,String department) {
 		// Next Id
 //		long nextAvailableId = course_Map.size() + 1;
@@ -20,41 +37,38 @@ public class CourseService {
 		Long key = Long.parseLong(courseId);
 		course_Map.put(key, course);
 	}
-
+*/
 	public Course addCourse(Course course) {
-		// Next Id
-//		long nextAvailableId = course_Map.size() + 1;
 		Long key = Long.parseLong(course.getCourseId());
-		course_Map.put(key, course);
+		course_Map.save(course);
 		return course;
 	}
 
 	// get the list of all courses
 	public List<Course> getAllCourses() {
-		ArrayList<Course> list = new ArrayList<>();
-		for (Course course : course_Map.values()) {
-			list.add(course);
-		}
-		return list;
+		return course_Map.scan(Course.class, new DynamoDBScanExpression());
 	}
 	// Getting one course
 
 	public Course getCourse(String courseId) {
 
-		// Simple HashKey Load
-		Long key = Long.parseLong(courseId);
-		Course course = course_Map.get(key);
-		System.out.println("Course Item retrieved:");
-		return course;
+		List<Course> list = course_Map.scan(Course.class, new DynamoDBScanExpression());
+		for (Course f : list) {
+			if (f.getCourseId().equals(courseId)) {
+				System.out.println("Course found");
+				return f;
+			}
+		}
+		return null;
 	}
 
 	// get courses by a department
 	public Course getCourseByDepartment(String department) {
-		// Simple HashKey Load
-
-		for (Course course : course_Map.values()) {
-			if (course.getDepartment().equals(department)) {
-				return course;
+		List<Course> list = course_Map.scan(Course.class, new DynamoDBScanExpression());
+		for (Course f : list) {
+			if (f.getDepartment().equals(department)) {
+				System.out.println("Found courses");
+				return f;
 			}
 		}
 		return null;
@@ -62,29 +76,44 @@ public class CourseService {
 
 	// get courses by a professorID
 	public Course getCourseByProfessor(String professorID) {
-		// Simple HashKey Load
-		for (Course course : course_Map.values()) {
-			if (course.getProfessorId().equals(professorID)) {
-				return course;
+		List<Course> list = course_Map.scan(Course.class, new DynamoDBScanExpression());
+		for (Course f : list) {
+			if (f.getProfessorId().equals(professorID)) {
+				System.out.println("Course taught by professor is:");
+				return f;
 			}
 		}
 		return null;
 	}
 
 	// Updating course Info
-	public Course updateCourseInformation(String courseID, Course course) {
-		Long key = Long.parseLong(courseID);
-		Course oldCourseObject = course_Map.get(key);
-		if (oldCourseObject != null) {
-		oldCourseObject = course;
-		course_Map.put(key, oldCourseObject);
+	public Course updateCourseInformation(String courseId, Course course) {
+		Map<String, AttributeValue> map = new HashMap<>();
+		map.put(":courseId", new AttributeValue().withS(courseId));
+		DynamoDBScanExpression scanExpression=
+		new DynamoDBScanExpression().withFilterExpression("courseId=:courseId").withExpressionAttributeValues(map);
+		List<Course> target = course_Map.scan(Course.class, scanExpression);
+		if(target.size()!=0) {
+			String Id = target.get(0).getCourseId();
+			course.setCourseId(courseId);
+			course_Map.save(course);
+			return course_Map.load(Course.class,Id);
 		}
-		return oldCourseObject;
+		return null;
 	}
 
 	// deleting course Info
 	public Course deleteCourse(String courseID) {
-		Long key = Long.parseLong(courseID);
-		return course_Map.remove(key);
+		Map<String, AttributeValue> map = new HashMap<>();
+		map.put(":courseId", new AttributeValue().withS(courseId));
+		DynamoDBScanExpression scanExpression=
+		new DynamoDBScanExpression().withFilterExpression("courseId=:courseId").withExpressionAttributeValues(map);
+		List<Course> target = course_Map.scan(Course.class, scanExpression);
+		if(target.size()!=0) {
+		course_Map.delete(target.get(0));
+			return target.get(0);
+		}
+		return null;
+		
 	}
 }
