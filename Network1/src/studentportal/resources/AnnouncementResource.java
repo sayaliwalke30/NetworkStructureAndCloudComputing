d737ebc485a7bcf41ed9df47b549f5f25d1b55c9
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-29 22:49:13 -0700

    DB

--- Network1/src/studentportal/resources/AnnouncementResource.java
+++ Network1/src/studentportal/resources/AnnouncementResource.java
@@ -1,6 +1,5 @@
 package studentportal.resources;
 
-
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -18,8 +17,6 @@ import studentportal.exception.RecordNotFoundException;
 import studentportal.services.AnnouncementService;
 import studentportal.services.BoardService;
 
-
-
 @Path("announcement")
 public class AnnouncementResource {
 	AnnouncementService a_service;
@@ -27,69 +24,73 @@ public class AnnouncementResource {
 	public AnnouncementResource() {
 		a_service = new AnnouncementService();
 	}
+
 	AnnouncementService bs = new AnnouncementService();
-	
-	//POST
-		@POST
-		@Produces(MediaType.APPLICATION_JSON)
-		@Consumes(MediaType.APPLICATION_JSON)
-		public Announcements addAnnouncement(Announcements announce) {
-			System.out.println("Posted object " + announce.toString());
-			return a_service.addAnnouncement(announce);
-		}
-	
-	//GET
+
+	// POST
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Announcements addAnnouncement(Announcements announce) {
+		System.out.println("Posted object " + announce.toString());
+		return a_service.addAnnouncement(announce);
+	}
+
+	// GET
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Announcements> getAllAnnoucements(){
+	public List<Announcements> getAllAnnoucements() {
 		System.out.println("In get announcement");
 		return a_service.getAllAnnouncements();
 	}
-	
-	//get an announcement by annoucementid and boardid
+
+	// get an announcement by annoucementid and boardid
 	@GET
 	@Path("/{boardId}_{announcementId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Announcements getOneAnnoucement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId) throws RecordNotFoundException {
+	public Announcements getOneAnnoucement(@PathParam("announcementId") String announcementId,
+			@PathParam("boardId") String boardId) throws RecordNotFoundException {
 		if (a_service.getAllAnnouncements().size() == 0) {
 			throw new RecordNotFoundException("Announcement not found");
 		}
 		return a_service.getOneAnnouncement(announcementId, boardId);
 	}
-	
-	//get announcements by boardId
+
+	// get announcements by boardId
 	@GET
 	@Path("/board/{boardId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Announcements> getAnnouncementsByBoard(@PathParam("boardId")String boardId) throws RecordNotFoundException{
+	public List<Announcements> getAnnouncementsByBoard(@PathParam("boardId") String boardId)
+			throws RecordNotFoundException {
 		if (a_service.getAllAnnouncements().size() == 0) {
 			throw new RecordNotFoundException("Announcement not found");
 		}
 		return a_service.getAnnoucementsByBoard(boardId);
 	}
-	//update
-		@PUT
-		@Path("/{boardId}_{annoucementId}")
-		@Produces(MediaType.APPLICATION_JSON)
-		@Consumes(MediaType.APPLICATION_JSON)
-		public Announcements updateAnnouncement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId,Announcements a) throws RecordNotFoundException {
-			if (a_service.getAllAnnouncements().size() == 0) {
-				throw new RecordNotFoundException("Announcement not found");
-			}
-			return a_service.updateAnnouncement(announcementId, boardId, a);
+
+	// update
+	@PUT
+	@Path("/{boardId}_{annoucementId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Announcements updateAnnouncement(@PathParam("announcementId") String announcementId,
+			@PathParam("boardId") String boardId, Announcements a) throws RecordNotFoundException {
+		if (a_service.getAllAnnouncements().size() == 0) {
+			throw new RecordNotFoundException("Announcement not found");
 		}
-	//delete
+		return a_service.updateAnnouncement(announcementId, boardId, a);
+	}
+
+	// delete
 	@DELETE
 	@Path("/{boardId}_{announcementId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Announcements deleteAnnoucement(@PathParam("announcementId")String announcementId,@PathParam("boardId")String boardId) throws RecordNotFoundException {
+	public Announcements deleteAnnoucement(@PathParam("announcementId") String announcementId,
+			@PathParam("boardId") String boardId) throws RecordNotFoundException {
 		if (a_service.getAllAnnouncements().size() == 0) {
 			throw new RecordNotFoundException("Announcement not found");
 		}
 		return a_service.deleteAnnouncement(announcementId, boardId);
 	}
-	
-	
-	
-	
+
 }
