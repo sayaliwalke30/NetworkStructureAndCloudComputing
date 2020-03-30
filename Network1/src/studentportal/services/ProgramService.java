50d36b2f1b72e776e0a9628b0014d5920a78d765
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-29 18:33:09 -0700

    DB

--- Network1/src/studentportal/services/ProgramService.java
+++ Network1/src/studentportal/services/ProgramService.java
@@ -3,71 +3,100 @@ package studentportal.services;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import studentportal.DynamoDbConnector;
 import studentportal.InMemoryDatabase;
+import studentportal.datamodel.Professor;
 import studentportal.datamodel.Program;
+import studentportal.datamodel.Student;
 
 public class ProgramService {
 	static DynamoDbConnector dynamoDb;
 	DynamoDBMapper program_map;
 	//static HashMap<Long, Program> program_map = InMemoryDatabase.getProgramDB();
+	public ProgramService() {
+	dynamoDb = new DynamoDbConnector();
+	dynamoDb.init();
+	program_map = new DynamoDBMapper(dynamoDb.getClient());
+	System.out.println("DynamoDb client initialized");
+	}
 
+	
+    // Add programs to database
+	public void addProgram(Program program) {
+		Long key = Long.parseLong(program.getProgramId());
+		System.out.print("Key is " + key);
+		program_map.save(program);
+	}
 	// get the list of all programs
-	public List<Program> getAllPrograms() {
-		ArrayList<Program> list = new ArrayList<>();
-		for (Program pro : program_map.values()) {
-			list.add(pro);
+		public List<Program> getAllPrograms() {
+			return program_map.scan(Program.class, new DynamoDBScanExpression());
 		}
-		return list;
-	}
-
-	public Program addProgram(Program program) {
-        Long key = Long.parseLong(program.getProgramId());
-		program_map.put(key, program);
-		return program;
-	}
 	// Getting one program
 
 	public Program getProgram(String programID) {
 
-		// Simple HashKey Load
-		Long key = Long.parseLong(programID);
-		Program program = program_map.get(key);
-		System.out.println("Item retrieved Programs:");
-
-		return program;
+		// Getting the list
+		List<Program> list = program_map.scan(Program.class, new DynamoDBScanExpression());
+		for (Program f : list) {
+			if (f.getProgramId().equals(programID)) {
+				System.out.println("Found programs");
+				return f;
+			}
+		}
+		return null;
+	}
+	// Get courses in a Program
+	public List<String> getCoursesByProgram(String progrmId) {
+		// Getting the list
+		ArrayList<String> listOfCourses = new ArrayList<>();
+		List<Program> list = program_map.scan(Program.class, new DynamoDBScanExpression());
+		for (Program program : list) {
+			if (program.getProgramId().equals(progrmId)) {
+				listOfCourses.addAll((program.getCoursesOfProgram()));
+			}
+		}
+		return listOfCourses;
 	}
 
 	// Updating Program Info
 	public Program updateProgramInformation(String programId, Program program) {
-		Long key = Long.parseLong(programId);
-		Program oldProgram = program_map.get(key);
-		if (oldProgram != null) {
-		oldProgram = program;
-		program_map.put(key, oldProgram);
+		Map<String, AttributeValue> map = new HashMap<>();
+		System.out.println("In update program " + programId);
+		map.put(":pogramId", new AttributeValue().withS(programId));
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				.withFilterExpression("programId=:programId").withExpressionAttributeValues(map);
+		List<Program> target = program_map.scan(Program.class, scanExpression);
+		if (target.size() != 0) {
+			String Id = target.get(0).getId();
+			System.out.println("The Id of given object is" + Id);
+			program.setId(Id);
+			program_map.save(program);
+			return program_map.load(Program.class, Id);
 		}
-		return oldProgram;
+		return null;
 	}
 
 	// Deleting a Program
 	public Program deleteProgram(String programId) {
-		Long key = Long.parseLong(programId);
-		Program deletedProgramDetails = program_map.remove(key);
-		return deletedProgramDetails;
+		Map<String, AttributeValue> map = new HashMap<>();
+		System.out.println("In update program " + programId);
+		map.put(":pogramId", new AttributeValue().withS(programId));
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				.withFilterExpression("programId=:programId").withExpressionAttributeValues(map);
+		List<Program> target = program_map.scan(Program.class, scanExpression);
+		if (target.size() != 0) {
+			program_map.delete(target.get(0));
+			return target.get(0);
+		}
+		return null;
+		
 	}
 
-	// Get courses in a Program
-	public List<String> getCoursesByProgram(String progrmId) {
-		// Getting the list
-		ArrayList<String> list = new ArrayList<>();
-		for (Program program : program_map.values()) {
-			if (program.getProgramId().equals(progrmId)) {
-				list.addAll((program.getCoursesOfProgram()));
-			}
-		}
-		return list;
-	}
+
 }
