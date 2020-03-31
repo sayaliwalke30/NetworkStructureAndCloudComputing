4c5a7d86a0041eb8904b66bb61c98ec4031957b8
Sayali Walke <47954666+sayaliwalke30@users.noreply.github.com>
2020-03-31 00:10:29 -0700

    DB

--- Network1/src/studentportal/DynamoDbConnector.java
+++ Network1/src/studentportal/DynamoDbConnector.java
@@ -1,4 +1,5 @@
 package studentportal;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -7,21 +8,23 @@ public class DynamoDbConnector {
 	static AmazonDynamoDB dynamoDb ;
 	 
 	 public static void init() {
+		 System.out.println(System.getenv("AWS_CREDENTIAL_PROFILES_FILE"));
 		if (dynamoDb == null) {
-		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
-		credentialsProvider.getCredentials();
+		//ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		//credentialsProvider.getCredentials();
 		
 		dynamoDb = AmazonDynamoDBClientBuilder
 					.standard()
-					.withCredentials(credentialsProvider)
+					.withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
 					.withRegion("us-west-2")
 					.build();		
 		System.out.println("I created the client");
 		} 
 
 	}
-	 
+	
 	 public AmazonDynamoDB getClient() {
+		 
 		 return dynamoDb;
 	 }
 }
